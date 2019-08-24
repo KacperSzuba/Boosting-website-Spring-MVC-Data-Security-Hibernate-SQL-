b31c44c623a4b804fecce96ff98078ad4c97e622
KacperSzuba <38376776+KacperSzuba@users.noreply.github.com>
2019-08-24 16:29:31 +0200

    Spring MVC BASE

---
+++ src/main/java/pl/javastart/config/WebConfig.java
@@ -0,0 +1,21 @@
+package pl.javastart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("pl.javastart")
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public InternalResourceViewResolver resolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
---
+++ src/main/java/pl/javastart/contoller/HomeController.java
@@ -0,0 +1,18 @@
+package pl.javastart.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String showHomePage(){
+        return "home";
+    }
+
+    @RequestMapping("/secure")
+    public String showSecuredPage(){
+        return "secure";
+    }
+}
