d6341997f84f3624e7796fa9708e1e74b0220623
KacperSzuba <38376776+KacperSzuba@users.noreply.github.com>
2019-09-23 18:07:13 +0200

    Connection to lol api.

--- src/main/java/pl/javastart/manage/LeagueOfLegendsAPIConnector.java
+++ src/main/java/pl/javastart/manage/LeagueOfLegendsAPIConnector.java
@@ -1,5 +1,7 @@
 package pl.javastart.manage;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import pl.javastart.model.entity.enums.Region;
@@ -9,27 +11,80 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class LeagueOfLegendsAPIConnector {
+    private Region region;
+    private String username;
+    private String tier;
+    private final String apiKey = "RGAPI-67e3f3cb-2f13-4118-a777-62cfd1bf83bb";
 
-    private URL url;
-    private BufferedReader reader;
-    private JsonParser parser;
-    private Object object;
-    private JsonObject jsonObject;
-    private Map<Region,String> map = new TreeMap<>();
-    public LeagueOfLegendsAPIConnector(String username ,Region region) throws MalformedURLException {
-        url = new URL("https://"+listOfRegions().get(region)+".api.riotgames.com/lol/summoner/v4/summoners/by-name/"+username+"?api_key=RGAPI-303dd9c3-95f5-485e-8a2a-034f3205b5a5");
+    LeagueOfLegendsAPIConnector(String username, Region region) throws MalformedURLException {
+        this.username = username;
+        this.region = region;
     }
 
-    public JsonObject connectWithLOLAPI() throws IOException {
-        reader = new BufferedReader(new InputStreamReader(url.openStream()));
-        parser = new JsonParser();
-        object = parser.parse(reader.readLine());
-        jsonObject = (JsonObject) object;
-        return jsonObject;
+    private Map<String,JsonObject> retrieveSummonerLeague() throws IOException {
+        URL summonerLeague = new URL("https://"+region()+".api.riotgames.com/lol/league/v4/entries/by-summoner/"+retrieveSummoner()+"?api_key="+apiKey);
+        JsonArray jsonArray = (JsonArray) parser(summonerLeague);;
+        Map<String,JsonObject> map = new HashMap<>();
+        for (JsonElement jsonElement : jsonArray) {
+            String key = jsonElement.getAsJsonObject().get("queueType").toString();
+            map.put(key,jsonElement.getAsJsonObject());
+        }
+        return map;
+    }
+
+    private String retrieveSummoner() throws IOException {
+        URL summoner = new URL("https://"+region()+".api.riotgames.com/lol/summoner/v4/summoners/by-name/"+username+"?api_key="+apiKey);
+        JsonObject jsonObject = (JsonObject) parser(summoner);
+        String str = jsonObject.get("id").toString();
+        return removeQuotes(str);
+    }
+
+    private Object parser(URL url) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+        JsonParser parser = new JsonParser();
+        return parser.parse(reader.readLine());
+    }
+    private JsonObject getJsonObject(String queueType) throws IOException {
+        return retrieveSummonerLeague().get(queueType);
+    }
+
+    public String getActualSoloDuoTier() throws IOException {
+        String tier = getJsonObject("\"RANKED_SOLO_5x5\"").get("tier").toString();
+        return removeQuotes(tier);
+    }
+
+    public String getActualSoloDuoDivision() throws IOException {
+        String division = getJsonObject("\"RANKED_SOLO_5x5\"").get("rank").toString();
+        return removeQuotes(division);
+    }
+
+    public String getActualSoloDuoLeaguePoints() throws IOException {
+        String leaguePoints = getJsonObject("\"RANKED_SOLO_5x5\"").get("leaguePoints").toString();
+        return removeQuotes(leaguePoints);
+    }
+
+    public String getActual5vs5Tier() throws IOException {
+        String tier = getJsonObject("\"RANKED_FLEX_SR\"").get("tier").toString();
+        return removeQuotes(tier);
+    }
+
+    public String getActual5vs5Division() throws IOException {
+        String division = getJsonObject("\"RANKED_FLEX_SR\"").get("rank").toString();
+        return removeQuotes(division);
+    }
+
+    public String getActual5vs5LeaguePoints() throws IOException {
+        String leaguePoints = getJsonObject("\"RANKED_FLEX_SR\"").get("leaguePoints").toString();
+        return removeQuotes(leaguePoints);
+    }
+
+    private String removeQuotes(String text){
+        return text.replace("\"", "");
     }
 
     private Map<Region,String> listOfRegions(){
@@ -38,5 +93,9 @@ public class LeagueOfLegendsAPIConnector {
         listOfRegions2.put(Region.EUNE,"eun1");
         listOfRegions2.put(Region.TR,"tr1");
         return listOfRegions2;
+    }
+
+    private String region(){
+        return listOfRegions().get(region);
     }
 }
--- src/test/java/pl/javastart/manage/LeagueOfLegendsAPIConnectorTest.java
+++ src/test/java/pl/javastart/manage/LeagueOfLegendsAPIConnectorTest.java
@@ -1,21 +1,43 @@
 package pl.javastart.manage;
 
-import com.google.gson.JsonObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.javastart.model.entity.enums.Region;
+import pl.javastart.model.entity.enums.Tier;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LeagueOfLegendsAPIConnectorTest {
+    private LeagueOfLegendsAPIConnector leagueConnector;
 
-    @Test
-    void shouldBeReturnJSonFile() throws IOException {
-        LeagueOfLegendsAPIConnector leagueOfLegendsAPIConnector = new LeagueOfLegendsAPIConnector("xVoleRx", Region.EUW);
-        System.out.println(leagueOfLegendsAPIConnector.connectWithLOLAPI());
-        // String showName = jsonObject.get("name").toString();
+    @BeforeEach
+    void initializeSummonerLeagueConnector() throws MalformedURLException {
+        leagueConnector = new LeagueOfLegendsAPIConnector("xVoleRx",Region.EUW);
     }
 
+    @Test
+    void tiersShouldBeCorrect() throws IOException {
+        //then
+        assertThat(leagueConnector.getActualSoloDuoTier(), is(Tier.DIAMOND.toString()));
+        assertThat(leagueConnector.getActual5vs5Tier(), is(Tier.SILVER.toString()));
+    }
+
+    @Test
+    void divisionsShouldBeCorrect() throws IOException {
+        //then
+        assertThat(leagueConnector.getActualSoloDuoDivision(),is("IV"));
+        assertThat(leagueConnector.getActual5vs5Division(),is("III"));
+    }
+
+    @Test
+    void leaguePointsShouldBeCorrent() throws IOException {
+        //then
+        assertThat(leagueConnector.getActualSoloDuoLeaguePoints(),is("75"));
+        assertThat(leagueConnector.getActual5vs5LeaguePoints(),is("53"));
+    }
 }
