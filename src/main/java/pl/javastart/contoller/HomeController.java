6aa2268c8e4c54b5dabc0c158586ac0b56287cfe
KacperSzuba <kacperszuba1999@gmail.com>
2020-01-13 11:43:21 +0100

    Refactoring home controller.

--- src/main/java/pl/javastart/contoller/HomeController.java
+++ src/main/java/pl/javastart/contoller/HomeController.java
@@ -1,8 +1,5 @@
 package pl.javastart.contoller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,16 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
-@PropertySource(value = "classpath:test.properties")
 public class HomeController {
-    Environment environment;
-
-    @Value("${test}")
-    private String test;
 
     @RequestMapping("/")
     public String showHomePage(){
-        System.out.println(test);
         return "jsp/home";
     }
 
