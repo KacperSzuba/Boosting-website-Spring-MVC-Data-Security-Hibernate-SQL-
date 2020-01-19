2c9556ef690142ffea2afab7fbbf574a96a9452d
KacperSzuba <kacperszuba1999@gmail.com>
2020-01-19 10:36:03 +0100

    Refactor of tier shifting mechanism to javascript.

--- src/main/java/pl/javastart/contoller/rest/OrderRestController.java
+++ src/main/java/pl/javastart/contoller/rest/OrderRestController.java
@@ -1,16 +1,16 @@
 package pl.javastart.contoller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.javastart.manage.json.JsonOrderHandler;
 
-@Controller
+@RestController
 public class OrderRestController {
 
     @Autowired
     private JsonOrderHandler jsonOrderHandler;
-    
+
     @RequestMapping("/order/Order")
     public String showOrderPage(){
         return jsonOrderHandler.createJsonFile().toJSONString();
