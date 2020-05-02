31399252e993526d3eebb1f107f14ef2470ef534
KacperSzuba <kacperszuba1999@gmail.com>
2020-05-02 17:45:49 +0200

    Adding new front end.

--- src/main/java/com/BoostingWebsite/account/password/reset/PasswordManagerController.java
+++ src/main/java/com/BoostingWebsite/account/password/reset/PasswordManagerController.java
@@ -18,7 +18,7 @@ public class PasswordManagerController {
 
     @GetMapping("/change/password")
     public String showChangePasswordPage(){
-        return "accountView/user_ChangePassword";
+        return "accountView/change-password";
     }
 
     @GetMapping("/change/password/form")
@@ -26,7 +26,7 @@ public class PasswordManagerController {
                                  @RequestParam("repeatPassword")String repeatPassword, Model model){
         passwordManager.changePassword(currentPassword, password, repeatPassword);
         model.addAttribute("newPassword",passwordManager.getMessage());
-        return "accountView/user_ChangePassword";
+        return "accountView/change-password";
     }
 
 }
