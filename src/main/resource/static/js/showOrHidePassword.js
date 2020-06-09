1bce61534bf2da373961ddf9df07990655f63bd7
KacperSzuba <kacperszuba1999@gmail.com>
2020-06-09 19:25:28 +0200

    Adding hide and show password

---
+++ src/main/resource/static/js/showOrHidePassword.js
@@ -0,0 +1,16 @@
+const input = document.querySelectorAll('input.password');
+const spanList = document.querySelectorAll('span.password');
+
+for (let i = 0; i < spanList.length; i++) {
+    spanList[i].addEventListener("click", function() {
+        spanList[i].classList.toggle('fa-eye-slash');
+        spanList[i].classList.toggle('fa-eye');
+
+        if (this.classList.contains('fa-eye-slash')) {
+            input[i].type = 'password';
+        } else {
+            input[i].type = 'text';
+        }
+
+    });
+}
