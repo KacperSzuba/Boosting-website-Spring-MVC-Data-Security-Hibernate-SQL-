398eb37fe0c75e93be7d37816cc111b4604f5896
KacperSzuba <kacperszuba1999@gmail.com>
2020-09-12 13:20:27 +0200

    Refactoring front-end

--- src/main/resource/static/js/navigation-animation-mobile.js
+++ src/main/resource/static/js/navigation-animation-mobile.js
@@ -5,9 +5,21 @@ const iconX = document.querySelector(".fa-times");
 const column = document.querySelector("aside");
 const header = document.querySelector("header");
 
+const accountDescription = document.querySelector("#account a");
+const subMenu = document.querySelector(".sub-menu");
+const hideSubMenu = document.querySelector(".back-to-main-menu");
+
 burger.addEventListener('click', function () {
     iconBurger.classList.toggle("show");
     iconX.classList.toggle("show");
     column.classList.toggle("show");
     header.classList.toggle("hide");
+});
+
+accountDescription.addEventListener('click', function () {
+    subMenu.classList.add("show-sub-menu");
+});
+
+hideSubMenu.addEventListener('click', function () {
+   subMenu.classList.remove("show-sub-menu");
 });
