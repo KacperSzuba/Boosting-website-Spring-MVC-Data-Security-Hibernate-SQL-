b7f65c8390b750f4614a73b272cf6f7754d24739
KacperSzuba <kacperszuba1999@gmail.com>
2020-10-20 21:29:21 +0200

    Refactoring home page

--- src/main/java/com/BoostingWebsite/controller/HomeController.java
+++ src/main/java/com/BoostingWebsite/controller/HomeController.java
@@ -2,15 +2,17 @@ package com.BoostingWebsite.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("/")
-    public String showHomePage()  {
+    @GetMapping
+    public String showHomePage(){
         return "home";
     }
 
