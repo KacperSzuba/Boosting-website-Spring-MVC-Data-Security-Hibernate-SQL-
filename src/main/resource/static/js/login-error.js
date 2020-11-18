61da3b2320ac402fe0d298b4c749f26fed60e18d
KacperSzuba <kacperszuba1999@gmail.com>
2020-11-18 16:13:50 +0100

    Improving login error

---
+++ src/main/resource/static/js/login-error.js
@@ -0,0 +1,11 @@
+const loginPageURL = window.location.href;
+const errorBanner = document.querySelector('#login-error');
+
+window.addEventListener('load', () => {
+    if(loginPageURL.includes('error')){
+        errorBanner.classList.add('show-login-error');
+    }
+    else{
+        errorBanner.classList.remove('show-login-error');
+    }
+});
