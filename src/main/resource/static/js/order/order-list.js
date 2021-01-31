a9b019e260d1c922537e5de8213fc5a388af71ad
KacperSzuba <kacperszuba1999@gmail.com>
2021-01-31 14:45:10 +0100

    Improving style of free orders page.

--- src/main/resource/static/js/order/order-list.js
+++ src/main/resource/static/js/order/order-list.js
@@ -1,1 +1,3 @@
 const showDetails = document.querySelectorAll('.show-details');
+
+//TODO - DLa wolnych zamówień i histori zamówień stworzyć rozwijaną listę zamiast na sztywno pokazanych wszystkich wartości
